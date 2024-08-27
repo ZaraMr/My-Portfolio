ea219106c27ff4e1bec5d89e085e523c1d185fcf
ZaraMr <zhra.mrdi2907@gmail.com>
2024-08-27 14:04:44 +0330

    Added JS to Project

---
+++ assets/Scripts/script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// const copyBtnEmail = document.querySelector(".icon-email");
+// const copyBtnNum = document.querySelector(".icon-num");
+
+// const textContent = document.querySelector(".contact-info-email").textContent;
+// const textNum = Number(
+//   document.querySelector(".contact-info-number").textContent
+// );
+
+// copyBtnEmail.addEventListener("click", () => {
+//   let textValue = textContent.value;
+//   navigator.clipboard.writeText(textValue);
+// });
+
+// copyBtnNum.addEventListener("click", () => {
+//   let numValue = textNum;
+//   navigator.clipboard.writeText(numValue);
+// });
+
+const navContainer = document.querySelector(".nav__container");
+const navLinks = document.querySelectorAll(".nav__link");
+const navLinksMarker = document.querySelectorAll(".marker");
+const navListActive = document.querySelector(".navbar__active");
+const sections = document.querySelectorAll("section");
+
+navContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".nav__link");
+
+  if (!clicked) return;
+
+  // Remove active classes
+  navLinks.forEach((link) => link.classList.remove("navbar__active"));
+  sections.forEach((s) => s.classList.remove("section--active"));
+
+  // Add active classes
+  clicked.classList.add("navbar__active");
+  document
+    .querySelector(`.section__${clicked.dataset.link}`)
+    .classList.add("section--active");
+});
+
+console.log(sections);
