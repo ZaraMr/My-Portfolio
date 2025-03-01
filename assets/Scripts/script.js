49756a9a4cfe98239ae15c137596e61ca7eea07b
ZaraMr <zhra.mrdi2907@gmail.com>
2025-03-01 21:52:36 +0330

    Change Navar

--- assets/Scripts/script.js
+++ assets/Scripts/script.js
@@ -23,21 +23,25 @@ const navLinks = document.querySelectorAll(".nav__link");
 const navLinksMarker = document.querySelectorAll(".marker");
 const navListActive = document.querySelector(".navbar__active");
 const sections = document.querySelectorAll(".section");
+const mobileNavContainer = document.querySelector(".nav__container");
+const navbars = document.querySelectorAll(".navbar");
 
-navContainer.addEventListener("click", function (e) {
-  const clicked = e.target.closest(".nav__link");
+navbars.forEach((navbar) => {
+  navbar.addEventListener("click", function (e) {
+    const clicked = e.target.closest(".nav__link");
 
-  if (!clicked) return;
+    if (!clicked) return;
 
-  // Remove active classes
-  navLinks.forEach((link) => link.classList.remove("navbar__active"));
-  sections.forEach((s) => s.classList.remove("section--active"));
+    // Remove active classes
+    navLinks.forEach((link) => link.classList.remove("navbar__active"));
+    sections.forEach((s) => s.classList.remove("section--active"));
 
-  // Add active classes
-  clicked.classList.add("navbar__active");
-  document
-    .querySelector(`.section__${clicked.dataset.link}`)
-    .classList.add("section--active");
+    // Add active classes
+    clicked.classList.add("navbar__active");
+    document
+      .querySelector(`.section__${clicked.dataset.link}`)
+      .classList.add("section--active");
+  });
 });
 
 // console.log(sections);
